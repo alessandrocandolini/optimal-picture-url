844f18d59f41ae02061e00cbf352d5e435c86414
Alessandro Candolini <alessandrocandolini@gmail.com>
2021-01-10 14:38:03 +0000

    One more PBT and restructure of the test code

--- test/OptimalImageSelectorSpec.hs
+++ test/OptimalImageSelectorSpec.hs
@@ -37,14 +37,28 @@ forAllValues p = all p . values
 isNotEmpty :: Map k a -> Bool
 isNotEmpty = not . M.null
 
+{-
+ Notice: at a deeper level, information-preserving properties (ie, empty/non-empty inputs and the fact that the output is always one of the inputs) are implied by functor laws for Maybe and polymorphism. NonEmpty allows us to embed precondition predicates into types.
+
+ So, polymorpism, refined types and lifting to functor contexts allows us to have most of these properties for free, and we could even consider not testing them.
+ The core specific part of the algorithm (the part where we should focus more) is represented by the optimisation properties
+-}
+
 spec :: Spec
 spec =
   describe "choosePicture" $ do
-    prop "should return no picture IF no picture is provided as input" $
-      \w -> isNothing (choosePicture w (PictureData M.empty))
+    describe "be information preserving, ie:" $ do
+      prop "should return no picture IF no picture is provided as input" $
+        \w -> isNothing (choosePicture w (PictureData M.empty))
 
-    prop "should return no picture ONLY IF there are no input pictures (ie, always return somethign when there are input pictures)" $
-      \w m -> isNotEmpty m ==> isJust (choosePicture w (PictureData m))
+      prop "should return no picture ONLY IF there are no input pictures (ie, always return somethign when there are input pictures, ie we don't filter values)" $
+        \w m -> isNotEmpty m ==> isJust (choosePicture w (PictureData m))
+
+      prop "should return one of the input values,  when it returns values (this property is a theorem, thanks to polymorphism)" $
+        \w m ->
+          isNotEmpty m
+            ==> let p = fromJust $ choosePicture w (PictureData m)
+                 in (any (== p) . values) m
 
     prop "should always return the picture matching the desired width, whenever there is one and only one picture in the input that matches the expected size" $
       \w h u k m ->
